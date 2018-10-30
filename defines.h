a9ac9d9033b34d3e9f17e4c0d8852440189cd4e5
xgallom <gallo.milan.jr@gmail.com>
2018-10-30 11:59:17 +0100

    Add gitignore and defines
    Signed-off-by: xgallom <gallo.milan.jr@gmail.com>

---
+++ defines.h
@@ -0,0 +1,14 @@
+//
+// Created by xgallom on 10/28/18.
+//
+
+#ifndef CH8C_DEFINES_H
+#define CH8C_DEFINES_H
+
+#define SUCCESS 0
+#define ERROR_ARGUMENTS 1
+#define ERROR_PARSING 2
+
+#define ARRAY_MAX 0x10
+
+#endif //CH8C_DEFINES_H
