ea6255cbda630579e3d54b85f064dd9702da4ee2
xgallom <gallo.milan.jr@gmail.com>
2018-10-30 11:58:28 +0100

    Add scope data type
    Signed-off-by: xgallom <gallo.milan.jr@gmail.com>

---
+++ x/scope.h
@@ -0,0 +1,20 @@
+//
+// Created by xgallom on 10/29/18.
+//
+
+#ifndef CH8C_SCOPE_H
+#define CH8C_SCOPE_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+namespace x
+{
+	struct scope {
+		string scope_data;
+		vector<size_t> scope_indexes;
+	};
+}
+
+#endif //CH8C_SCOPE_H
